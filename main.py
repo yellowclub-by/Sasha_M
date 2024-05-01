790d31230c8b7078a2a7b106c097d3e5881ee176
YellowClub <hello@yellowclub.by>
2024-05-01 17:46:40 +0300

    lesson 1 - echo bot was created

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-print("первый проект Александра на python")
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+
+TOKEN = "7189697687:AAGB9yCldmyNwYbf3wsCp8Ed0IzOnnQfISk"
+bot = Bot(token=TOKEN)
+
+dp = Dispatcher()
 
 
+@dp.message(CommandStart())
+async def start(message: types.Message):
+    await message.answer("Привет, это бот по продаже ключей стим")
 
 
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer('Бот находится в разработке')
+    user_text = message.text
+    await message.answer(user_text)
 
+
+async def main():
+    print("бот запущен")
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
