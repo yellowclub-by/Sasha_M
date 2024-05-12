6523525a4882495dfc2b5ad53d655502e81a1d04
YellowClub <hello@yellowclub.by>
2024-05-12 12:56:18 +0300

    lesson 3 - Magic filters

--- handlers/user_privat.py
+++ handlers/user_privat.py
@@ -1,4 +1,4 @@
-from aiogram import types, Router
+from aiogram import types, Router, F
 from aiogram.filters import CommandStart, Command
 
 user_router = Router()
@@ -9,28 +9,36 @@ async def start(message: types.Message):
     await message.answer("Привет, это бот по продаже ключей стим")
 
 
+@user_router.message(F.text.lower() == "каталог")
 @user_router.message(Command('catalog'))
 async def catalog(message: types.Message):
     await message.answer('Вот наш каталог:')
 
 
+@user_router.message(F.text.lower() == "о нас")
 @user_router.message(Command('about'))
 async def about(message: types.Message):
     await message.answer("О нас:")
 
 
+@user_router.message(F.text.lower() == "контакты")
 @user_router.message(Command('contacts'))
 async def contacts(message: types.Message):
     await message.answer("Контакты:")
 
 
+@user_router.message(F.text.lower() == "филиалы")
 @user_router.message(Command('addresses'))
 async def addresses(message: types.Message):
     await message.answer("Филиалы:")
 
-
-@user_router.message()
-async def echo(message: types.Message):
-    await message.answer('Бот находится в разработке')
-    user_text = message.text
-    await message.answer(user_text)
+# @user_router.message(F.text)
+# @user_router.message(F.photo)
+# @user_router.message(F.text.lower() == "доставка")
+# @user_router.message(F.text.lower().contains('доставк'))
+# @user_router.message(F.text.lower().startswith('как'))
+# @user_router.message(F.text.endswith("?"))
+# @user_router.message(F.text.lower().startswith("как"), F.text.endswith('?'))
+# @user_router.message((F.text.lower().contains("стоимост")) | (F.text.lower().contains("цен")))
+# async def echo(message: types.Message):
+#     await message.answer("Сработал магический фильтр")
